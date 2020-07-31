29f651aee8d44dffbb6b60c693727b0b6fb46032
siri <bajjurishirisha@gmail.com>
2020-07-31 15:29:58 -0700

    Added additional checks...

--- src/com/ex/app/Test2app.java
+++ src/com/ex/app/Test2app.java
@@ -4,8 +4,12 @@ public class Test2app {
 
 	public static void main(String[] args) {
 		
+	String teammember="y";
+	   if(teammember=="y") {
+		   System.out.println("done by the team member");
+	   }
 		
-		System.out.println("done by the team member");
+		System.out.println("not done by the team member");
 	}
 
 }
